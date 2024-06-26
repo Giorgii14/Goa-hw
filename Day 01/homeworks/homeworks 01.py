cd7adcc152b420acef2e7a3ad1a4b4affbe4e7ca
Giorgii14 <burgeramakdonaldsi@gmail.com>
2024-06-26 14:28:57 +0400

    first commit

---
+++ Day 01/homeworks/homeworks 01.py
@@ -0,0 +1,15 @@
+name = "giorgi"
+surname = "gurgenidze"
+
+print(name)
+print(surname)
+age = 14
+height = 167
+know_programming = True
+print(age)
+print(height)
+
+
+
+
+print("my name is " +  name  + "  "  +  surname + " i am " + str(age))

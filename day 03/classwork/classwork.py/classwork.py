f81c22df9cf625ee0f0398608f0c91e9389b13f2
Giorgii14 <burgeramakdonaldsi@gmail.com>
2024-07-11 16:07:13 +0400

    fifth commit

---
+++ day 03/classwork/classwork.py/classwork.py
@@ -0,0 +1,28 @@
+name_boy =("giorgi")  # 1 davaleba
+name_girl =("mariami")
+print(name_boy)
+print(name_girl)
+
+
+
+year=2000     #2 davaleba
+movie=("titanic")
+
+
+
+print(year) #3 davaleba
+age_1=14
+age_2=15
+print(age_1 + age_2)
+
+
+#4 davaleba
+name=("giorgi" #shecdoma (aklia brchxili)
+
+print(name) 
+
+
+#5 davalevba
+name=("giorgi")
+surname=("gurgenidze")
+print(name + surname) #printshi shevkribe ertad rom gamoetana
---
+++ day 03/homework/day 03 .py/day 03.py
@@ -0,0 +1,43 @@
+ship_name = ("Titanic") #ორ სიტყვიანი ცვლადის დაწერის დროს სიტყვებს ერტმანეთა ვაშორებთ ქვედატირეთი (_)
+print(ship_name)    # 1 დავალება
+
+
+# 2 დავალება
+# 1 მაგალითი
+age_man = 30
+age_woman = 25
+total_age = ("age_man +age_woman")
+print(30 + 25)
+
+
+# 2 მაგალითი
+cat_weigh = 4
+dog_weigh = 6
+total_weigh = ("dog_weigh + cat_weigh")
+print(4 + 6)
+
+# 3 მაგალითი
+first_number = 20
+second_number = 4
+print(20 // 4)
+ 
+
+# 4 მაგალითი
+first_number = 17
+second_number = 2
+print(17 / 2)
+
+# 5 მაგალითი
+first_number = 100
+second_number = 3
+print(100 * 3)
+
+
+# 3 დავალება
+age = 20
+height = 180
+print(180 + 20 )
+print(180 - 20)
+print(180 * 20)
+print(180 / 20)
+print(180 // 20)

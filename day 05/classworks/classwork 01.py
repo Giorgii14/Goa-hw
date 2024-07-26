b9e3a516c58f80e317163dace6b42cf0361b3fd0
Giorgii14 <burgeramakdonaldsi@gmail.com>
2024-07-26 13:56:57 +0400

    third commit

---
+++ day 05/classworks/classwork 01.py
@@ -0,0 +1,13 @@
+name=input()
+print('hello is ' + name ) 
+
+name="Giorgi"
+age=14
+weight=55.5
+print(name)
+print(age)
+print(weight)
+
+
+
+
---
+++ day 05/homeworks/homework 03/homework 03.py
@@ -0,0 +1,16 @@
+#string  str
+name=("giorgi")
+
+#integer int
+age=14
+
+#float
+height=170.5
+
+
+print(name)
+
+print(age)
+
+
+print(height)

0c8817afde75f95ec148d9512410b723bf49a868
Giorgii14 <burgeramakdonaldsi@gmail.com>
2024-08-18 17:54:38 +0400

    tenth commit

---
+++ Day 08/homework/homework 11/homework 11.py
@@ -0,0 +1,12 @@
+boy_age = int(input("enter your age: "))
+father_age = int(input("enter your father's age: "))
+print(f"15 წელში მამაშენი იქნება {father_age + 15} წლის, შენ იქნები {boy_age + 15} წლის. მამაშენი იქნება {(father_age + 15)/(boy_age + 15)}-ჯერ დიდი მამაშენი შენზე.")           
+
+
+
+
+
+
+
+
+
